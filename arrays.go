f5fe15524d9f6d09cc2add5e7151f6a8eee8dbba
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-03-27 16:24:37 +0300

    arrays

---
+++ arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr1 = [3]int{1,2,3}
+	arr2 := [5]int{4,5,6,7,8}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	////////
+	var arr3 = [...]int{1,2,3}
+	arr4 := [...]int{4,5,6,7,8}
+
+	fmt.Println(arr3)
+	fmt.Println(arr4)
+
+	////////
+	var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+	fmt.Print(cars)
+}
